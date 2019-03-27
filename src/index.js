38ef73e6307f7c17a3749b38e23eff9a86c7c457
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2019-03-27 12:51:51 -0400

    add dynamic port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
 const app = express();
-process.env.PORT = 3000;
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.status(200).send('Hello world!');
 });
