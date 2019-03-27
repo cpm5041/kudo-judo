bf823a471f3c5e61d26b9e91ed3467319da6858d
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2019-03-27 13:51:15 -0400

    add slack depency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const slack = require('slack');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.status(200).send('Hello world!');
+});
+app.get('/messages', (req, res) => {
+  const messages = [];
+  res.status(200).send(messages);
 });
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
