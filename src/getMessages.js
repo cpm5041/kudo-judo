41c8d732f8f502584190b887a89cf095524905ce
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2019-03-28 11:12:44 -0400

    add get userinfo

--- src/getMessages.js
+++ src/getMessages.js
@@ -8,7 +8,17 @@ const obj = {
   channel: 'CHDNY8J0M',
 };
 
-const getKudosMessages = allMessages => allMessages.map(message => ({ text: message.text, user: message.user })).filter(message => message.text.includes('<#CHDNY8J0M|kudos>'));
+const getUserInfo = async (userId) => {
+  const res = await web.users.info({
+    token: process.env.SLACK_TOKEN,
+    user: userId,
+  });
+  return res.user;
+};
+
+const getKudosMessages = allMessages => allMessages
+  .map(async message => ({ text: message.text, user: await getUserInfo(message.user), ts: message.ts }))
+  .filter(message => message.text.includes('<#CHDNY8J0M|kudos>'));
 
 const messages = async () => {
   const res = await web.channels.history({
@@ -17,6 +27,7 @@ const messages = async () => {
   });
   return getKudosMessages(res.messages);
 };
+
 
 module.exports = {
   messages,
