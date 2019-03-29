0aab33a06b332ad6a1aafede9735c9dbed212f17
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2019-03-29 16:21:23 -0400

    add proper message which gets some recipients

--- src/getMessages.js
+++ src/getMessages.js
@@ -2,11 +2,14 @@ const { WebClient } = require('@slack/client');
 
 const web = new WebClient(process.env.SLACK_TOKEN);
 
+const userMap = {};
+
 const getUserInfo = async (userId) => {
   const res = await web.users.info({
     token: process.env.SLACK_TOKEN,
     user: userId,
   });
+  userMap[userId] = res.user.real_name;
   return {
     name: res.user.real_name,
     image: res.user.profile.image_192,
@@ -15,10 +18,26 @@ const getUserInfo = async (userId) => {
 
 const getKudosRecipient = async (message) => {
   const userId = message.substr(message.indexOf('@') + 1, 9); // need to get userId from message.text
-  console.log('message', message);
-  console.log('user id', userId);
   const recipient = await getUserInfo(userId);
   return recipient;
+};
+
+const parseUserId = userId => userId.slice(2, userId.length - 1);
+
+const getProperMessage = (message) => {
+  const stringArray = message.split(' ');
+  for (let i = 0; i < stringArray.length; i++) {
+    const phrase = stringArray[i];
+
+    if (phrase.match(/<@[A-Z0-9]{0,9}>/)) {
+      const userId = parseUserId(stringArray[i]);
+      if (!userMap[userId]) {
+        getUserInfo(userId);
+      }
+      stringArray[i] = userMap[userId];
+    }
+  }
+  return stringArray.join(' ');
 };
 
 const getKudosMessages = async (allMessages) => {
@@ -26,9 +45,10 @@ const getKudosMessages = async (allMessages) => {
     .filter(message => message.text.includes('<#CHDNY8J0M|kudos>') && message.text.includes('@'))
     .map(async (message) => {
       const timestamp = new Date(parseInt(message.ts.split('.')[0], 10));
+      const userInfo = await getUserInfo(message.user);
       return {
-        text: message.text,
-        author: await getUserInfo(message.user),
+        text: getProperMessage(message.text),
+        author: userInfo,
         timestamp,
         recipient: await getKudosRecipient(message.text),
       };
@@ -37,6 +57,7 @@ const getKudosMessages = async (allMessages) => {
   const parsedMessages = await Promise.all(messagesWithUserInfo);
   return parsedMessages;
 };
+
 
 const messages = async () => {
   const res = await web.channels.history({
