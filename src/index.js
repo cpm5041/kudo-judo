59307ef84194ab0a7fad7c5623d5ddc9ff4941d8
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2019-04-01 15:41:59 -0400

    add CORS headers to incoming requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const getMessages = require('./getMessages');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://kudos-wallboard.herokuapp.com');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello world!');
 });
