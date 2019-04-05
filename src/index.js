334b6e76f264f26fb6dbae223808fd062e60a1b5
ALEX LEO <alexander.leo@libertymutual.com>
2019-04-05 13:04:50 -0400

    allow more headers because heroku

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ app.use(basicAuth({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://kudos-wallboard.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization');
   next();
 });
 
