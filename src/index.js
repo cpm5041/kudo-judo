a2d63db8de04a61ab5d1a3f36c9fac13726cf49f
ALEX LEO <alexander.leo@libertymutual.com>
2019-04-05 13:32:39 -0400

    allow all the things

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(basicAuth({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://kudos-wallboard.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization');
