4a1370ade78119857d18419e41f0031532b75e21
ALEX LEO <alexander.leo@libertymutual.com>
2019-04-05 13:00:22 -0400

    allow credentials and GET method

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ app.use(basicAuth({
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://kudos-wallboard.herokuapp.com');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
