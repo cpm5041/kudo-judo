60c4a4fe6a7b40acfe0d9de72acdfd1e4d548d88
ALEX LEO <alexander.leo@libertymutual.com>
2019-04-05 10:32:32 -0400

    add basic auth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
 require('dotenv').config();
 const getMessages = require('./getMessages');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(basicAuth({
+  users: { kudojudo: 'liberty1' },
+}));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://kudos-wallboard.herokuapp.com');
