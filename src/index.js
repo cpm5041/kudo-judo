6bb79c192bb1b65ea797743aa2a33983b04618e2
ALEX LEO <alexander.leo@libertymutual.com>
2019-04-05 12:47:41 -0400

    allow Authorization headers

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(basicAuth({
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://kudos-wallboard.herokuapp.com');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
